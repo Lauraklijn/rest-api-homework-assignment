abc333ba6bc38a2edc498742ae13ae2ef49b0a82
Lauraklijn <lauraklijn12@hotmail.com>
2020-02-08 17:56:38 +0100

    Final version

--- messages-api.js
+++ messages-api.js
@@ -1,16 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const port = process.env.PORT || 3000;
+// 1. Create a new JS file named `messages-api.js`.
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// 2. Create an Express app in that file.
+// The app should listen for requests on port `3000`.
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// 4. When a request is sent to the endpoint, it should log the `text` property of the body to the console, and it should respond with a JSON object, for example:
+//    In order to parse the JSON body of the request, you will need to add the middleware for it.
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.post("/", (request, response, next) => {
-  console.log(request.body);
+// 3. Add a single endpoint to the app responds to `POST` requests to the `/messages`.
+app.post("/messages", (request, response, next) => {
+  if (!request.body.text || request.body.text === "") {
+    response.status(400).send("Bad Request"); //5. Perform the following validation: if the body does NOT have a `text` property or the string is empty, then send a "Bad Request" HTTP status code to the client.
+  } else {
+    console.log(request.body.text);
+  }
   response.json({
     message: "This is the message that was sent!"
   });
+  next(error);
 });
 
-app.listen(port, () => console.log("listening on port " + port));
+app.listen(port, () => console.log(`listening on port: ${port}`));
+
+// 6. The API should only log the message five times.
+// After receiving five messages, sixth request should be sent a response that indicates the HTTP status for "Too Many Requests".
+// Make sure the correct HTTP status code is sent (Google it if you haven't seen this status message before).
+// --> I broke my code after trying this, unfortunately I can't get it to work
+// 7. Put the message limit logic from the previous step into a middleware function. It should behave the same.
--- sequelize-rest.js
+++ sequelize-rest.js
@@ -1,10 +1,21 @@
+// 1. Create a new JavaScript file named `sequelize-rest.js`.
+// 2. Install the dependency `sequelize@5.8.6` - Installed
+
+// 3. In the JavaScript file, initialize the database connection with Sequelize.
 const Sequelize = require("sequelize");
 const databaseUrl =
   process.env.DATABASE_URL ||
   "postgres://postgres:secret@localhost:5432/postgres";
-const db = new Sequelize(databaseUrl);
+const dataBase = new Sequelize(databaseUrl);
+const port = process.env.PORT || 4000;
+// --------------------------------------
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-const Movie = db.define("movies", {
+// 4. Using Sequelize, define a model called `Movie` with the following properties (in addition to an ID):
+// - `title` (text), `yearOfRelease` (number), `synopsis` (text)
+const Movie = dataBase.define("movies", {
   title: {
     type: Sequelize.STRING,
     field: "movie_title"
@@ -19,10 +30,13 @@ const Movie = db.define("movies", {
   }
 });
 
-db.sync()
-  .then(() => console.log("database updated")) // add a then callback that sends newly created team as the response
-  .catch(console.error); //add catch callback where you pass any caught errors to next
+//5. Make sure the model is synched with the database upon startup.
+dataBase
+  .sync({ force: false }) //--> To clean database (first: true, then false)
+  .then(() => console.log("database updated"))
+  .catch(console.error);
 
+// 6. Use the model `create()` method to insert 3 rows of example data. This logic should happen _after_ the model synchronization completes. The data should persist. Restarting the API should not cause any data to be lost.
 const movieData = Movie.create(
   {
     title: "Titanic",
@@ -42,6 +56,78 @@ const movieData = Movie.create(
     })
   )
 );
-movieData.then(movie => {
-  console.log("user", movie.get());
+
+app.use(bodyParser.json());
+
+//7. Create an express app with routes that support the following RESTful actions on the "movies" resources.
+// 1. create a new movie resource
+// 2. read all movies (the collections resource)
+// 3. read a single movie resource
+// 4. update a single movie resource
+// 5. delete a single movie resource
+
+// 1. Create a new movie resource --> Use post
+app.post("/movies", (request, response, next) => {
+  Movie.create(request.body)
+    .then(movie => response.json(movie))
+    .catch(error => next(error));
 });
+
+// 2. Read all movies --> Use get
+// Implement pagination(9.) on the "read all" collections resource end-point (user must be able to use limit & offset).
+app.get("/movies", (request, response, next) => {
+  const limit = request.query.limit || 5;
+  const offset = reqest.query.offset || 0;
+
+  Movie.findAndCountAll({ limit, offset })
+    .then(resultPerPage =>
+      response.send({ Data: resultPerPage.rows, Total: resultPerPage.count })
+    )
+    .catch((error = next(error)));
+});
+
+// 3. Read a single movie resource --> Use get
+app.get("/movies/:id", (request, response, next) => {
+  Movie.findByPK(request, params.id)
+    .then(movie => {
+      if (!movie) {
+        response.status(404).end();
+      } else {
+        response.json(movie);
+      }
+    })
+    .catch(error => next(error));
+});
+
+// 4. Update a single movie resource --> Use put
+app.put("/movies/:id", (request, response, next) => {
+  Movie.findByPk(request.params.id)
+    .then(movie => {
+      if (movie) {
+        movie.update(req.body).then(movie => response.json(movie));
+      } else {
+        response.status(404).end(); //8. Make sure that your handlers send back `404` status codes.
+      }
+    })
+    .catch(error => next(error));
+});
+
+// 5. Delete a single movie resource --> Use delete
+app.delete("/movie/:id", (request, response, next) => {
+  Movie.destroy({ where: { id: request.params.id } })
+    .then(num => {
+      if (num) {
+        response.send({ message: "Movie is deleted" });
+      } else {
+        response.status(404).end(); //8. Make sure that your handlers send back `404` status codes.
+      }
+    })
+    .catch(error => next(error));
+});
+
+// Call port:
+app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+// 10. Make sure that all endpoints handle database errors in the promise chain. Errors should be handled by Express' built-in error handler.
+
+// I got errors by running http in terminal, I could not find out why.
